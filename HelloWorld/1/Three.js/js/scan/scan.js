5ff69cb6b0d5240ec66442a967d586d7647a0732
songqi <921233883@qq.com>
2017-08-04 11:12:04 +0800

    圈子

---
+++ HelloWorld/1/Three.js/js/scan/scan.js
@@ -0,0 +1,39 @@
+/**
+ * 二维码扫描
+ */
+function Scan(){
+	var that = this;
+	that.scan = new plus.barcode.Barcode('bcid');
+	that.scan.onmarked = onmarked; 
+	function onmarked( type, result ) {
+	var text = '未知: ';
+	switch(type){
+		case plus.barcode.QR:
+		text = 'QR: ';
+		break;
+		case plus.barcode.EAN13:
+		text = 'EAN13: ';
+		break;
+		case plus.barcode.EAN8:
+		text = 'EAN8: ';
+		break;
+	}
+	alert( text+result );
+	}
+}
+
+/**
+ * 开始扫描
+ */
+Scan.prototype.startScan = function(){
+	var that = this;
+	that.scan.start();
+}
+Scan.prototype.cancelScan = function() {
+	var that = this;
+	that.scan.cancel();
+}
+Scan.prototype.setFlash = function() {
+	var that = this;
+	that.scan.setFlash();
+}
