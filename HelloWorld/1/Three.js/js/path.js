3227bbd7748f8bc26035662c65461971aa5cc01b
huyibo <869015686@qq.com>
2017-09-29 17:32:50 +0800

    吐槽记录列表

--- HelloWorld/1/Three.js/js/path.js
+++ HelloWorld/1/Three.js/js/path.js
@@ -1,1 +1,16 @@
 var JS_PATH="http://120.25.221.94:8080";//本项目所有ajax，域名配置
+//调用ajax方法
+var humanAjax = function(url,obj){
+	mui.ajax(url,{
+					data:obj.data,
+					crossDomain :obj.crossDomain,
+					dataType:obj.dataType,//服务器返回json格式数据
+					type:obj.type,//HTTP请求类型
+					success:function(data){
+						obj.success(data);
+						
+					},error:function(e){
+						obj.error(e);
+					}
+				});
+}
